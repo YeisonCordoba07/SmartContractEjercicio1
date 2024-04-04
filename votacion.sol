e971ca95c2e5795e3bcdc90742c4bd838514a8bf
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-04-04 18:12:43 -0500

    agregar direcciones

--- votacion.sol
+++ votacion.sol
@@ -8,6 +8,8 @@ contract Counter{
         uint votos;
     }
 
+    address public administrador;
+
     address[] public listaDirecciones;
     Propuesta[] public listaPropuestas;
 
@@ -15,11 +17,23 @@ contract Counter{
     constructor (address _direccion){
         //0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
         listaDirecciones.push(_direccion);
-        crearPropuesta("1) Yeison");
-        crearPropuesta("2) Camila");
-        crearPropuesta("3) Laura");
-        crearPropuesta("4) Sara");
-        crearPropuesta("5) John G");
+        crearPropuesta("(0) BLANCO");
+        crearPropuesta("(1) Yeison");
+        crearPropuesta("(2) Camila");
+        crearPropuesta("(3) Laura");
+        crearPropuesta("(4) Sara");
+        crearPropuesta("(5) John G");
+
+        administrador = 0x94d3595B283309b080F4d036BF9239D1fDC606F4;
+
+        listaDirecciones.push(0x94d3595B283309b080F4d036BF9239D1fDC606F4);
+        listaDirecciones.push(0x925BcAe7e1b70d235DF9B6927F0071169708dDf8);
+        listaDirecciones.push(0x0255bF566828fB0ecd70C18dC4875021373fBED8);
+        listaDirecciones.push(0x60BF6307f6C3261B2863373B0CbC8f2742Ef00f1);
+        listaDirecciones.push(0xc3EFc68bf06655C7EfA1f0a4DBa8F14193752458);
+        listaDirecciones.push(0x0b01Bb34cE3d64d737A2668eb934C93c420999b1);
+
+
     }
 
 
@@ -27,7 +41,7 @@ contract Counter{
 
     function crearPropuesta(string memory nombrePropuesta) public {
 
-        require(listaDirecciones.length > 0 && msg.sender == listaDirecciones[0], "Solo la primera direccion puede agregar propuestas");
+        require(msg.sender == administrador, "Solo el administrador puede agregar propuestas");
         Propuesta memory nuevaPropuesta = Propuesta({
             nombre: nombrePropuesta,
             votos: 0
@@ -45,7 +59,7 @@ contract Counter{
 
 
     function votar(uint256 posicionPropuesta) public {
-        uint posicionDireccion;
+        uint posicionDireccion = 0;
         for(uint i = 0; i < listaDirecciones.length; i++){
             if(msg.sender == listaDirecciones[i]){
                 listaPropuestas[posicionPropuesta].votos = listaPropuestas[posicionPropuesta].votos + 1;
@@ -62,7 +76,10 @@ contract Counter{
 
 
 
-
+    function obtenerDirecciones() public view returns(address[] memory){
+        return listaDirecciones;
+    }
+    
     function obtenerPropuestas() public view returns(Propuesta[] memory){
         return listaPropuestas;
     }
