6a023d20b575fee98a0ef77b5bb88db5700b0f17
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-04-04 23:24:20 -0500

    agregar estructura de votantes

--- votacion.sol
+++ votacion.sol
@@ -8,15 +8,21 @@ contract Counter{
         uint votos;
     }
 
+    struct Votante{
+        address direccionVotante;
+        bool haVotado;
+    }
+
     address public administrador;
 
-    address[] public listaDirecciones;
     Propuesta[] public listaPropuestas;
+    Votante[] public listaVotantes;
 
 
-    constructor (address _direccion){
-        //0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
-        listaDirecciones.push(_direccion);
+    constructor (){
+
+        //listaDirecciones.push(msg.sender);
+        administrador = msg.sender;
         crearPropuesta("(0) BLANCO");
         crearPropuesta("(1) Yeison");
         crearPropuesta("(2) Camila");
@@ -24,16 +30,44 @@ contract Counter{
         crearPropuesta("(4) Sara");
         crearPropuesta("(5) John G");
 
-        administrador = 0x94d3595B283309b080F4d036BF9239D1fDC606F4;
-
-        listaDirecciones.push(0x94d3595B283309b080F4d036BF9239D1fDC606F4);
-        listaDirecciones.push(0x925BcAe7e1b70d235DF9B6927F0071169708dDf8);
+        //listaDirecciones.push(0x94d3595B283309b080F4d036BF9239D1fDC606F4);
+        /*listaDirecciones.push(0x925BcAe7e1b70d235DF9B6927F0071169708dDf8);
         listaDirecciones.push(0x0255bF566828fB0ecd70C18dC4875021373fBED8);
         listaDirecciones.push(0x60BF6307f6C3261B2863373B0CbC8f2742Ef00f1);
         listaDirecciones.push(0xc3EFc68bf06655C7EfA1f0a4DBa8F14193752458);
-        listaDirecciones.push(0x0b01Bb34cE3d64d737A2668eb934C93c420999b1);
+        listaDirecciones.push(0x0b01Bb34cE3d64d737A2668eb934C93c420999b1);*/
 
+        Votante memory nuevoVotante0 = Votante({
+            direccionVotante: 0x94d3595B283309b080F4d036BF9239D1fDC606F4,
+            haVotado: false
+        });
+        Votante memory nuevoVotante1 = Votante({
+            direccionVotante: 0x925BcAe7e1b70d235DF9B6927F0071169708dDf8,
+            haVotado: false
+        });
+        Votante memory nuevoVotante2 = Votante({
+            direccionVotante: 0x0255bF566828fB0ecd70C18dC4875021373fBED8,
+            haVotado: false
+        });
+        Votante memory nuevoVotante3 = Votante({
+            direccionVotante: 0x60BF6307f6C3261B2863373B0CbC8f2742Ef00f1,
+            haVotado: false
+        });
+        Votante memory nuevoVotante4 = Votante({
+            direccionVotante: 0xc3EFc68bf06655C7EfA1f0a4DBa8F14193752458,
+            haVotado: false
+        });
+        Votante memory nuevoVotante5 = Votante({
+            direccionVotante: 0x0b01Bb34cE3d64d737A2668eb934C93c420999b1,
+            haVotado: false
+        });
 
+        listaVotantes.push(nuevoVotante0);
+        listaVotantes.push(nuevoVotante1);
+        listaVotantes.push(nuevoVotante2);
+        listaVotantes.push(nuevoVotante3);
+        listaVotantes.push(nuevoVotante4);
+        listaVotantes.push(nuevoVotante5);
     }
 
 
@@ -41,43 +75,40 @@ contract Counter{
 
     function crearPropuesta(string memory nombrePropuesta) public {
 
-        require(msg.sender == administrador, "Solo el administrador puede agregar propuestas");
-        Propuesta memory nuevaPropuesta = Propuesta({
-            nombre: nombrePropuesta,
-            votos: 0
-        });
-        listaPropuestas.push(nuevaPropuesta);
+        //require(msg.sender == listaVotantes[0].direccionVotante, "Solo el administrador puede agregar propuestas");
+        if(msg.sender == administrador){
+            Propuesta memory nuevaPropuesta = Propuesta({
+                nombre: nombrePropuesta,
+                votos: 0
+            });
+            listaPropuestas.push(nuevaPropuesta);
+        }
+
     }
 
 
-    event LogDireccion(address direccion);
+    event LogDireccion(string texto,address direccion);
 
     function imprimirDireccion() public {
-        emit LogDireccion(msg.sender);
+        emit LogDireccion("direccion22", msg.sender);
     }
 
 
 
     function votar(uint256 posicionPropuesta) public {
-        uint posicionDireccion = 0;
-        for(uint i = 0; i < listaDirecciones.length; i++){
-            if(msg.sender == listaDirecciones[i]){
+        for(uint i = 0; i < listaVotantes.length; i++){
+            if(msg.sender == listaVotantes[i].direccionVotante && listaVotantes[i].haVotado == false){
                 listaPropuestas[posicionPropuesta].votos = listaPropuestas[posicionPropuesta].votos + 1;
-                posicionDireccion = i;
+                listaVotantes[i].haVotado = true;
+                break;
             }
         }
-
-        //Elimina la dirección de la lista de direcciones para que no pueda votar mas
-        for(uint i = posicionDireccion; i < listaDirecciones.length - 1; i++){
-            listaDirecciones[i] = listaDirecciones[i + 1];
-        }
-        listaDirecciones.pop();
     }
 
 
 
-    function obtenerDirecciones() public view returns(address[] memory){
-        return listaDirecciones;
+    function obtenerVotantes() public view returns(Votante[] memory){
+        return listaVotantes;
     }
     
     function obtenerPropuestas() public view returns(Propuesta[] memory){
